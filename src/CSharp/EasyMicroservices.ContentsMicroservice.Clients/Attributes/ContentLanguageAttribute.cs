65c8dfe3cbbcc88b7c0bb3c73146cd75a9292333
Mahdiyar Ghannad <m.gh.1385.mahdiyar@gmail.com>
2023-08-23 18:59:38 +0330

    Add ContentLanguageHelper

---
+++ src/CSharp/EasyMicroservices.ContentsMicroservice.Clients/Attributes/ContentLanguageAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EasyMicroservices.ContentsMicroservice.Clients.Attributes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ContentLanguageAttribute : Attribute
+    {
+    }
+}
